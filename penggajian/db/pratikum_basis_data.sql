762bc28abb94a856237122195bff976a5c5944df
haris ardani <96418570+harisardani18630181@users.noreply.github.com>
2022-01-30 17:28:01 +0700

    praktikum

---
+++ penggajian/db/pratikum_basis_data.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2022 at 11:22 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pratikum_basis_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bagian`
+--
+-- Error reading structure for table pratikum_basis_data.bagian: #1932 - Table 'pratikum_basis_data.bagian' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.bagian: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`bagian`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bagian_karyawan`
+--
+-- Error reading structure for table pratikum_basis_data.bagian_karyawan: #1932 - Table 'pratikum_basis_data.bagian_karyawan' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.bagian_karyawan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`bagian_karyawan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jabatan`
+--
+-- Error reading structure for table pratikum_basis_data.jabatan: #1932 - Table 'pratikum_basis_data.jabatan' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.jabatan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`jabatan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jabatan_karyawan`
+--
+-- Error reading structure for table pratikum_basis_data.jabatan_karyawan: #1932 - Table 'pratikum_basis_data.jabatan_karyawan' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.jabatan_karyawan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`jabatan_karyawan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+-- Error reading structure for table pratikum_basis_data.karyawan: #1932 - Table 'pratikum_basis_data.karyawan' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.karyawan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`karyawan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+-- Error reading structure for table pratikum_basis_data.lokasi: #1932 - Table 'pratikum_basis_data.lokasi' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.lokasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`lokasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penggajian`
+--
+-- Error reading structure for table pratikum_basis_data.penggajian: #1932 - Table 'pratikum_basis_data.penggajian' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.penggajian: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`penggajian`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+-- Error reading structure for table pratikum_basis_data.pengguna: #1932 - Table 'pratikum_basis_data.pengguna' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.pengguna: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`pengguna`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presensi`
+--
+-- Error reading structure for table pratikum_basis_data.presensi: #1932 - Table 'pratikum_basis_data.presensi' doesn't exist in engine
+-- Error reading data for table pratikum_basis_data.presensi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pratikum_basis_data`.`presensi`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ penggajian/partials/footer.php
@@ -0,0 +1,8 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
+  </footer>
