8090f8b2f56ee2c1ef23745944f0b7a288bce035
haris ardani <96418570+harisardani18630181@users.noreply.github.com>
2022-02-06 23:38:56 +0700

    Update routes.php

--- penggajian/routes.php
+++ penggajian/routes.php
@@ -2,8 +2,43 @@
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch($page) {
+        case'bagianread':
+            file_exists('pages/admin/bagianread.php') ? include 'pages/admin/bagianread.php' : include 'pages/404.php';
+            break;
+        case'bagiancreate':
+            file_exists('pages/admin/bagiancreate.php') ? include 'pages/admin/bagiancreate.php' : include 'pages/404.php';
+            break;
+        case'bagianupdate':
+            file_exists('pages/admin/bagianupdate.php') ? include 'pages/admin/bagianupdate.php' : include 'pages/404.php';
+            break;
+        case'bagiandelete':
+            file_exists('pages/admin/bagiandelete.php') ? include 'pages/admin/bagiandelete.php' : include 'pages/404.php';
+            break;
+
+        case'jabatanread':
+            file_exists('pages/admin/jabatanread.php') ? include 'pages/admin/jabatanread.php' : include 'pages/404.php';
+            break;
+        case'jabatancreate':
+            file_exists('pages/admin/jabatancreate.php') ? include 'pages/admin/jabatancreate.php' : include 'pages/404.php';
+            break;
+        case'jabatanupdate':
+            file_exists('pages/admin/jabatanupdate.php') ? include 'pages/admin/jabatanupdate.php' : include 'pages/404.php';
+            break;
+        case'jabatandelete':
+            file_exists('pages/admin/jabatandelete.php') ? include 'pages/admin/jabatandelete.php' : include 'pages/404.php';
+            break;
+
         case'lokasiread':
             file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include 'pages/404.php';
+            break;
+        case'lokasicreate':
+            file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include 'pages/404.php';
+            break;
+        case'lokasiupdate':
+            file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include 'pages/404.php';
+            break;
+        case'lokasidelete':
+            file_exists('pages/admin/lokasidelete.php') ? include 'pages/admin/lokasidelete.php' : include 'pages/404.php';
             break;
         case 'home':
             file_exists('pages/home.php') ? include 'pages/home.php' : include 'pages/404.php';
